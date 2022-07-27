a1b868c7048434aa0c68250103d5a00d5425f535
Kashish Jiandani <kashishjiandani@gmail.com>
2022-07-27 15:46:27 +0530

    kj

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from turtle import title
 from flask import Flask,render_template,redirect,request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import tkinter as tk
+import tkinter as TK
 
 
 from sqlalchemy import desc
